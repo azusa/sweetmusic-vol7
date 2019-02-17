c869712e9b79b9ca2fed4f908ce4f5b29da466a7
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2019-02-17 15:49:23 +0900

    スクリプト整形

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,8 @@ TARGET="sweetmusic-vol7"
 #curl -X POST -f -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
 #	-D - -H "Dropbox-API-Arg: {\"path\": \"/${TARGET}.pdf\",\"mode\": \"overwrite\",\"mute\": true}" -H "Content-Type: application/octet-stream" --data-binary @target/${TARGET}.pdf https://content.dropboxapi.com/2/files/upload
 curl -X POST -f -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
-	-D - -H "Dropbox-API-Arg: {\"path\": \"/${TARGET}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" -H "Content-Type: application/octet-stream" --data-binary @target/${TARGET}.pdf https://content.dropboxapi.com/2/files/upload
+	-D - -H "Dropbox-API-Arg: {\"path\": \"/${TARGET}-${TIMESTAMP}.pdf\",\"mode\": \"overwrite\",\"mute\": false}" \
+	-H "Content-Type: application/octet-stream" \
+	--data-binary @target/${TARGET}.pdf \
+	https://content.dropboxapi.com/2/files/upload
 
