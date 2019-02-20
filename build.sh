88376850de3654dd29a59f0135e967942998015b
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2019-02-20 22:48:03 +0900

    jsdomをビルド時に追加

--- build.sh
+++ build.sh
@@ -40,6 +40,7 @@ mkdir -p target/img
 cp -f src/*.css src/*.otf src/*.ttf target/
 cp -f src/img/* target/img
 
+npm install jsdom
 node rewrite.js target/${OUTPUT}.html target/${OUTPUT}-rewrite.html
 
 savepdf --no-sandbox -s JIS-B5  -o target/${OUTPUT}.pdf target/${OUTPUT}-rewrite.html
